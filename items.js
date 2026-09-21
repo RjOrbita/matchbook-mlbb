2e569beca973ae2351dd670098ddea5cf143691c
RJOrbita <rolandjayorbita@gmail.com>
2026-09-21 12:36:50 +0800

    Implement Player Profiles, Item Multiselect Dropdowns, and verify KDA format

---
+++ items.js
@@ -0,0 +1,21 @@
+/**
+ * Complete list of MLBB items for dropdown suggestions.
+ */
+const MLBB_ITEMS = [
+  // Movement
+  "Warrior Boots", "Tough Boots", "Magic Shoes", "Arcane Boots", "Swift Boots", "Rapid Boots", "Demon Shoes",
+  
+  // Physical
+  "Sea Halberd", "Rose Gold Meteor", "Bloodlust Axe", "Hunter Strike", "Blade of Despair", "Blade of the Heptaseas", 
+  "Windtalker", "Endless Battle", "Berserker's Fury", "Haas's Claws", "Malefic Roar", "War Axe", "Wind of Nature", 
+  "Golden Staff", "Corrosion Scythe", "Demon Hunter Sword", "Great Dragon Spear", "Sky Piercer",
+  
+  // Magic
+  "Genius Wand", "Lightning Truncheon", "Fleeting Time", "Blood Wings", "Clock of Destiny", "Starlium Scythe", 
+  "Glowing Wand", "Ice Queen Wand", "Concentrated Energy", "Holy Crystal", "Divine Glaive", "Necklace of Durance", 
+  "Feather of Heaven", "Winter Truncheon", "Enchanted Talisman", "Wishing Lantern", "Flask of the Oasis",
+  
+  // Defense
+  "Radiant Armor", "Twilight Armor", "Bruteforce Breastplate", "Immortality", "Dominance Ice", "Athena's Shield", 
+  "Oracle", "Antique Cuirass", "Guardian Helmet", "Cursed Helmet", "Thunder Belt", "Queen's Wings", "Blade Armor"
+].sort();
